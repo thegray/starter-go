4be36e66052bdc8ceb646de896f9d94a16dd3eab
paulus.bangun <pbangunk@gmail.com>
2025-04-05 21:28:00 +0700

    add context for repo functions

--- internal/domain/example/repository.go
+++ internal/domain/example/repository.go
@@ -1,6 +1,8 @@
 package example
 
+import "context"
+
 type ExampleRepository interface {
-	FindByID(id int) (*Example, error)
-	Save(example *Example) error
+	FindByID(ctx context.Context, id int) (*Example, error)
+	Save(ctx context.Context, example *Example) error
 }
