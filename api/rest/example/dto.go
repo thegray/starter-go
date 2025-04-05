4b065b619882e545db956c7af010fb47177e9146
paulus.bangun <pbangunk@gmail.com>
2025-04-05 21:15:14 +0700

    request validation to use go validation library

--- api/rest/example/dto.go
+++ api/rest/example/dto.go
@@ -3,7 +3,7 @@ package example
 import "starter-go/internal/domain/example"
 
 type CreateExampleRequest struct {
-	Description string `json:"description"`
+	Description string `json:"description" binding:"required,min=1,max=100"`
 }
 
 type ExampleResponse struct {
